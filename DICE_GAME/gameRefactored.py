420d2771a10ae8f947fd5ffba1e927b8a29a007a
makzprog <makzprog@gmail.com>
2025-12-09 23:09:35 +0100

    Encapsulation edits for better readability

--- DICE_GAME/gameRefactored.py
+++ DICE_GAME/gameRefactored.py
@@ -4,57 +4,62 @@ class Dice:
     def __init__(self, dices=2):
         if dices < 1:
             raise ValueError("Please choose atleast 1 die")
-        self._sides = 6
         self._dices = dices
-        
-    def get_amount(self):
-        return self._dices
-        
-    def set_amount(self, value):
+        self._sides = 6
+    
+    @property
+    def dices(self):
+        return self._dices    
+    
+    @property
+    def sides(self):
+        return self._sides
+    
+    @dices.setter    
+    def dices(self, value):
         if value < 1:
             raise ValueError("please choose atleast 1 dice")
         self._dices = value
         
 class Game:
     def __init__(self, dice):
-        self._dice = dice
+        self.dice = dice
+        self.roll = 0
+        self.count = 0
+        self.sum_total = 0
             
     def get_user_input(self):
         user_input = input("How many dices do you want to roll? ")
         if user_input.isdigit() and int(user_input) > 0:
-            self._dice.set_amount(int(user_input))
-            return int(user_input)
+            self.dice.dices = int(user_input)
+            return f"You have chosen: {user_input}"
         else:
             raise ValueError("please choose atleast 1 dice")
-        
-dice = Dice()
-game = Game(dice)
-print(game.get_user_input())
-print(f"Number of dices: {dice.get_amount()}")
-
-
-
-
-      
-"""
-dice = Dice(6)
-print(f"Number of sides: {dice.get_sides()}")
-print(f"Number of dices: {dice.get_amount()}")
-
-dice.set_sides(12)
-print(f"Number of sides: {dice.get_sides()}")
-print(f"Number of dices: {dice.get_amount()}")
-
-dice.set_amount(12)
-print(f"Number of sides: {dice.get_sides()}")
-print(f"Number of dices: {dice.get_amount()}")    
-
-"""
     
+    def roll_dices(self):
+        while True:
+            ask_user = input("Roll the dices? (y/n): ").lower()
+            if ask_user == 'y':
+                self.roll = tuple(random.randint(1, self.dice.sides) for _ in range(self.dice.dices))
+                self.count += 1
+                self.sum_total += sum(self.roll)
+                print("You rolled:", self.roll, " | Total:", self.sum_total, " | Times Rolled:", self.count)
+                self.sum_total = 0
+            elif ask_user == 'n':
+                print("Game Over")
+                print("Total rolls:",  self.count)
+                break
+            else:
+               print("Invalid input. Please enter 'y' or 'n'.")
+        return "Thanks for playing!"
+    
+if __name__ == "__main__":
+    dice = Dice()
+    game = Game(dice)
+    print(game.get_user_input())
+    print(game.roll_dices())
 
 
 
-
-  
 
 
